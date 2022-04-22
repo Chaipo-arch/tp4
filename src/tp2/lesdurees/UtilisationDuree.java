94c8c6db6c49597ff48c24f94c4b0c5bafa2bbe8
Chaipo <ahmed.bribach@iut-rodez.fr>
2022-04-22 15:21:07 +0200

    séparation des tests de la classe duree et amélioration de la   classe de tests

--- src/tp2/lesdurees/UtilisationDuree.java
+++ src/tp2/lesdurees/UtilisationDuree.java
@@ -4,16 +4,74 @@
 
 package tp2.lesdurees;
 
+import java.util.Scanner;
+
 /**
  * Utilisation des méthodes traitant les durées
  */
 public class UtilisationDuree {
+    
+    
+    /**
+     * TODO commenter le rôle de cette méthode (SRP)
+     *
+     */
+    public static void testCombienDifferente() {
+        int[] d1; // première durée
+        int[] d2; // deuxième durée
+        int[] d3; // troisième
+
+        d1 = DureeHMS.saisirDuree("Saisie de la première durée : ");
+        d2 = DureeHMS.saisirDuree("Saisie de la deuxième durée : ");
+        d3 = DureeHMS.saisirDuree("Saisie de la troisième durée : ");
+
+        System.out.println("Parmi ces 3 durées, "
+                + DureeHMS.combienDifferentes(d1, d2, d3)
+                + " sont différentes.");
+
+    }
+    
+    /**
+     * TODO commenter le rôle de cette méthode (SRP)
+     *
+     */
+    public static void testOrdre() {
+        int[] d1; // première durée
+        int[] d2; // deuxième durée
+        int[] d3; // troisième
+
+        d1 = DureeHMS.saisirDuree("Saisie de la première durée : ");
+        d2 = DureeHMS.saisirDuree("Saisie de la deuxième durée : ");
+        d3 = DureeHMS.saisirDuree("Saisie de la troisième durée : ");
+
+        System.out.println("ces 3 durées sont "
+                + (DureeHMS.ordre(d1, d2, d3) ? " ordonnées " : " dans le désordre"));
+
+    }
+
+  
+    /**
+     * TODO commenter le rôle de cette méthode (SRP)
+     *
+     */
+    public static void testEstChaineDuree() {
+        Scanner entree = new Scanner(System.in);
+        String phrase;
+        System.out.print("Entrez une chaine : ");
+        phrase = entree.nextLine();
+        if (DureeHMS.estChaineDuree(phrase)) {
+            System.out.println("OK !!");
+        } else {
+            System.out.println("ERREUR !!! ");
+        }
+    }
+
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         DureeHMS.testChaine();
+         testEstChaineDuree();
     }
 
 }
