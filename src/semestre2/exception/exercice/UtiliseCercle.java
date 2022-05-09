da4b0efcc147699af0202f9f1532d9c2ee78940f
Chaipo <ahmed.bribach@iut-rodez.fr>
2022-05-09 15:28:35 +0200

    premier commit

--- src/semestre2/exception/exercice/UtiliseCercle.java
+++ src/semestre2/exception/exercice/UtiliseCercle.java
@@ -25,8 +25,9 @@ public class UtiliseCercle {
         Scanner entree = new Scanner(System.in);
 
         double rayon;           // rayon du cercle entré par l'utilisateur
-               
-
+        double diametre;        // diamétre du cercle calculé a l'aide du rayon 
+        double hauteur;         // hauteur du cylindre entré par l'utilisateur
+        
         // saisie du rayon du cercle
         System.out.print("Donnez le rayon du cercle : ");
         rayon = entree.nextDouble();
@@ -35,8 +36,20 @@ public class UtiliseCercle {
         System.out.println("Le perimetre du cercle est "
                            + Cercle.perimetre(rayon) 
                            + "\nLa surface du disque est "
-                           + Cercle.surface(rayon));       
-
+                           + Cercle.surface(rayon) );   
+        
+        
+        
+        // saisie de la hauteur du cylindre et du diametre du cyclindre
+        System.out.print("\nDonnez le diametre du cylindre : ");
+        diametre = entree.nextDouble();
+        System.out.print("Donnez la hauteur du cylindre : ");
+        hauteur = entree.nextDouble();
+        
+        
+        // affichage des résultats : du volume du cylindre
+        System.out.println("Le volume du cylindre est "
+                           + Cercle.volume(diametre, hauteur));
     }
 
 }
